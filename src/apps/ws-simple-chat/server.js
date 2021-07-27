4669d3fce081caa99bcf7aa4370fcdda757a1f74
Vadym Kinchur <vkincur@gmail.com>
2021-07-27 12:06:12 +0300

    Set up server to serve static files

--- src/apps/ws-simple-chat/server.js
+++ src/apps/ws-simple-chat/server.js
@@ -1,11 +1,27 @@
 'use strict';
 
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const PORT = 8080;
+const STATIC_DIR = './static';
+
+const httpError = (code, res) => {
+  const message = http.STATUS_CODES[code];
+  res.writeHead(code).end(message);
+};
 
 const server = http.createServer((req, res) => {
-  res.end('<h1>WS Chat</h1>');
+  const { method, url } = req;
+  if (method === 'GET') {
+    const fileName = url.substring(1) || 'index.html';
+    const filePath = path.join(STATIC_DIR, fileName);
+    fs.readFile(filePath, (err, content) => {
+      if (err) httpError(404, res);
+      res.end(content);
+    });
+  } else httpError(405, res);
 });
 
 server.listen(PORT, () => {
